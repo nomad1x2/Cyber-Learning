2cd42f98c978ac7aeb6aa9cf6840eae20ad42ebf
Bugz <nomad1x2sec@gmail.com>
2026-05-19 07:43:22 -0500

    Update dir_check.sh

--- 05-scripting-projects/bash/dir_check.sh
+++ 05-scripting-projects/bash/dir_check.sh
@@ -6,7 +6,7 @@ FILE=specific_file
 ############
 
 echo -e "Checking if $DIR exists:\n"
-if [ ! -d $DIR ]
+if [[ ! -d $DIR ]]
 then
 	echo -e "$DIR does not exist. Creating $DIR:\n"
 	mkdir $DIR
@@ -16,7 +16,7 @@ fi
 
 ############
 
-if [ ! -f $DIR/$FILE ]
+if [[ ! -f $DIR/$FILE ]]
 then
 	echo -e "$FILE does not exist. Creating $FILE:\n"
 	touch $DIR/$FILE
